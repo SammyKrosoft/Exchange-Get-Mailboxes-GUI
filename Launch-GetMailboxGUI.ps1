2ee26a23ed1da9750bb4eb939c0763c78c0f3104
Administrator <Administrator@CANADA.CA>
2018-11-11 01:10:19 -0500

    enhanced

--- Launch-GetMailboxGUI.ps1
+++ Launch-GetMailboxGUI.ps1
@@ -153,7 +153,7 @@ Function Run-Action{
                     $CSVClip = $mailboxFeatures | ConvertTo-CSV -NoTypeInformation
                     $CSVClip | clip.exe
                     $title = "Copied !"
-                    $msg = "Data copied to the clipboard ! `n`rUse CTRL+V on Notepad or on an Exchange Management Shell"
+                    $msg = "Data copied to the clipboard ! `n`rUse CTRL+V on Notepad or on Excel !"
                     [System.Windows.MessageBox]::Show($msg,$title, "OK","Asterisk")
                 })
                 $wpf.btnClose.add_Click({
@@ -255,7 +255,7 @@ Function Run-Action{
                     $CSVClip = $mailboxFeatures | ConvertTo-CSV -NoTypeInformation
                     $CSVClip | clip.exe
                     $title = "Copied !"
-                    $msg = "Data copied to the clipboard ! `n`rUse CTRL+V on Notepad or on an Exchange Management Shell"
+                    $msg = "Data copied to the clipboard ! `n`rUse CTRL+V on Notepad or on Excel !"
                     [System.Windows.MessageBox]::Show($msg,$title, "OK","Asterisk")
                 })
                 $wpf.btnClose.add_Click({
@@ -312,6 +312,20 @@ Function Ready-Label{
     $Wpf.$FormName.Dispatcher.Invoke("Render",[action][scriptblock]{})
 }
 
+Function Update-MainCommandLine {
+    If ($wpf.txtMailboxString.text -eq ""){
+        $SearchSubstring = ("*")
+    } Else {
+        $SearchSubstring = ("*") + ($wpf.txtMailboxString.text) + ("*")
+    }
+    $chkIncludeDiscovery = $false
+    If ($chkIncludeDiscovery){
+        $commandLine = "Get-Mailbox -ResultSize $($wpf.txtResultSize.Text) -Identity $SearchSubstring -ErrorAction Stop | Select Name,Alias,DisplayName,primarySMTPAddress"
+    } Else {
+        $commandLine = "Get-Mailbox -ResultSize $($wpf.txtResultSize.Text) -Identity $SearchSubstring -Filter {RecipientTypeDetails -ne `"DiscoveryMailbox`"} -ErrorAction Stop | Select Name,Alias,DisplayName,primarySMTPAddress"
+    }
+    $wpf.txtMainCommand.Text = $CommandLine
+}
 Function Get-Mailboxes {
     If ($([int]$wpf.txtResultSize.Text) -gt 1000) {Write-Host "$($wpf.txtResultSize.Text) is greater than 1000 ..."} Else {write-host "$($wpf.txtResultSize.Text) is less than 1000"}
     If ($([int]$wpf.txtResultSize.Text) -gt 1000){
@@ -333,37 +347,31 @@ Function Get-Mailboxes {
         $Answer = [System.Windows.MessageBox]::Show($msg,$Title, $Button, $icon)
         If($Answer -eq "No"){Return}
     }
-    $SearchSubstring = ("*") + ($wpf.txtMailboxString.text) + ("*")
     Try {
-        $chkIncludeDiscovery = $false
-        If ($chkIncludeDiscovery){
-            $Mailboxes = Get-Mailbox -ResultSize $($wpf.txtResultSize.Text) $SearchSubstring -ErrorAction Stop | Select Name,Alias,DisplayName,primarySMTPAddress
-        } Else {
-            #$Mailboxes = Get-Mailbox -ResultSize $($wpf.txtResultSize.Text) $SearchSubstring -Filter {RecipientTypeDetails -ne "DiscoveryMailbox"} -ErrorAction Stop | Select Name,Alias,DisplayName,primarySMTPAddress
-            $stopwatch = [system.diagnostics.stopwatch]::StartNew()
-            #$Mailboxes = Get-Mailbox $SearchSubstring -Filter {RecipientTypeDetails -ne "DiscoveryMailbox"} -ErrorAction Stop | Select -First $($wpf.txtResultSize.Text) | Select Name,Alias,DisplayName,primarySMTPAddress
-            $Mailboxes = Get-Mailbox -ResultSize $($wpf.txtResultSize.Text) $SearchSubstring -Filter {RecipientTypeDetails -ne "DiscoveryMailbox"} -ErrorAction Stop | Select Name,Alias,DisplayName,primarySMTPAddress
-            $stopwatch.Stop()
-            $msg = "`n`nInstruction took $([math]::round($($StopWatch.Elapsed.TotalSeconds),2)) seconds to retrieve $($Mailboxes.count) mailboxes..."
-            Write-Host $msg
-            $msg = $null
-            $StopWatch = $null
+        #Initiating stopwatch to measure the time it takes to retrieve mailboxes
+        $stopwatch = [system.diagnostics.stopwatch]::StartNew()
+        #Getting the command line from the text box where it's generated
+        $commandLine = $wpf.txtMainCommand.text
+        #Invoking the command line and storing in a variable
+        $Mailboxes = invoke-expression $CommandLine
+        #Stopping stopwatch
+        $stopwatch.Stop()
+        $msg = "`n`nInstruction took $([math]::round($($StopWatch.Elapsed.TotalSeconds),2)) seconds to retrieve $($Mailboxes.count) mailboxes..."
+        Write-Host $msg
+        $msg = $null
+        $StopWatch = $null
 
-        }
-
-        #$Processes = Get-process -Name $SearchSubstring -ErrorAction Stop 
+        #Populating the GridView
         [System.Collections.IENumerable]$Results = @($Mailboxes)
-        #[System.Collections.IENumerable]$Results = @($Processes)
         $wpf.GridView.ItemsSource = $Results
         $wpf.GridView.Columns | Foreach {
             $_.CanUserSort = $true
         }
-        #$wpf.lblStatus.Content = "Found $($Results.Count) Mailbox(es)"
         $wpf.lblNbItemsInGrid.Content = $($Results.Count)
-    }
-    Catch {
+    } Catch {
         $Mailboxes = $null
-        #$wpf.lblStatus.Content = "No mailboxes found... try again !"
+        $wpf.GridView.ItemsSource = $null
+        write-host "ZERO MAILBOXES"
         $wpf.lblNbItemsInGrid.Content = 0
     }
 }
@@ -380,7 +388,6 @@ $wpf = @{}
 # NOTE: Either load from a XAML file or paste the XAML file content in a "Here String"
 # $inputXML = Get-Content -Path "C:\Users\Kamehameha\Documents\GitHub\PowerShell\Get-EventsFromEventLog\VisualStudio2017WPFDesign\Launch-EventsCollector-WPF\Launch-EventsCollector-WPF\MainWindow.xaml"
 $inputXML = @"
-
 <Window x:Name="WForm" x:Class="GridView_WPF.MainWindow"
         xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
         xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
@@ -388,30 +395,55 @@ $inputXML = @"
         xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
         xmlns:local="clr-namespace:GridView_WPF"
         mc:Ignorable="d"
-        Title="Search Mailboxes" Height="450" Width="800" ResizeMode="NoResize">
+        Title="Search Mailboxes" Height="535.64" Width="800" ResizeMode="NoResize">
     <Grid>
-        <DataGrid x:Name="GridView" HorizontalAlignment="Left" Height="349" Margin="353,31,0,0" VerticalAlignment="Top" Width="410" Grid.ColumnSpan="2"/>
-        <TextBox x:Name="txtMailboxString" HorizontalAlignment="Left" Height="23" Margin="10,87,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="302"/>
-        <Label Content="Search for mailbox (substring of alias, e-mail address, &#xD;&#xA;display name, ...)" HorizontalAlignment="Left" VerticalAlignment="Top" Margin="10,31,0,0" Height="51" Width="302"/>
-        <Button x:Name="btnRun" Content="Search" HorizontalAlignment="Left" Margin="10,115,0,0" VerticalAlignment="Top" Width="75"/>
-        <Label x:Name="lblStatus" Content="Please start a search..." HorizontalAlignment="Left" VerticalAlignment="Top" Margin="10,189,0,0" Width="255" FontStyle="Italic"/>
-        <Button x:Name="btnAction" Content="Action" Margin="273,302,446,89.5" IsEnabled="False"/>
-        <ComboBox x:Name="comboSelectAction" HorizontalAlignment="Left" Margin="228,337,0,0" VerticalAlignment="Top" Width="120" Height="24" SelectedIndex="0" IsEnabled="False">
+        <DataGrid x:Name="GridView" HorizontalAlignment="Left" Height="385" Margin="353,10,0,0" VerticalAlignment="Top" Width="410"/>
+        <TextBox x:Name="txtMailboxString" HorizontalAlignment="Left" Height="23" Margin="10,67,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="338"/>
+        <Label Content="Search for mailbox (substring of alias, e-mail address, &#xD;&#xA;display name, ...)" HorizontalAlignment="Left" VerticalAlignment="Top" Margin="10,11,0,0" Height="51" Width="302"/>
+        <Button x:Name="btnRun" Content="Search" HorizontalAlignment="Left" Margin="10,95,0,0" VerticalAlignment="Top" Width="75" Height="32">
+            <Button.Effect>
+                <DropShadowEffect/>
+            </Button.Effect>
+        </Button>
+        <Label x:Name="lblStatus" Content="Please start a search..." HorizontalAlignment="Left" VerticalAlignment="Top" Margin="0,471,0,0" Width="784" FontStyle="Italic" FontWeight="Bold">
+        </Label>
+        <Button x:Name="btnAction" Content="Action on selected" Margin="353,430,250,41" IsEnabled="False">
+            <Button.Effect>
+                <DropShadowEffect/>
+            </Button.Effect>
+        </Button>
+        <ComboBox x:Name="comboSelectAction" HorizontalAlignment="Left" Margin="549,431,0,0" VerticalAlignment="Top" Width="214" Height="35" SelectedIndex="0" IsEnabled="False" TextOptions.TextFormattingMode="Display" VerticalContentAlignment="Center" HorizontalContentAlignment="Center">
+            <ComboBox.Effect>
+                <DropShadowEffect/>
+            </ComboBox.Effect>
             <ComboBoxItem Content="List Mailbox Features"/>
             <ComboBoxItem Content="List Single Item Recovery status"/>
             <ComboBoxItem Content="Disable Mailbox"/>
         </ComboBox>
-        <Label x:Name="lblNbItemsInGrid" Content="0" HorizontalAlignment="Left" VerticalAlignment="Top" Margin="506,385,0,0" Width="55"/>
-        <Label Content="Number of Items in Grid:" HorizontalAlignment="Left" Margin="353,385,0,0" VerticalAlignment="Top" Width="148"/>
-        <Label Content="Selected:" HorizontalAlignment="Left" Margin="621,385,0,0" VerticalAlignment="Top"/>
-        <Label x:Name="lblNumberItemsSelected" Content="0" HorizontalAlignment="Left" Margin="684,385,0,0" VerticalAlignment="Top"/>
-        <TextBox x:Name="txtResultSize" HorizontalAlignment="Left" Height="23" Margin="21,275,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="124" Text="100"/>
-        <TextBlock HorizontalAlignment="Left" Margin="21,238,0,0" TextWrapping="Wrap" Text="ResultSize (aka Nb of mailboxes to display):" VerticalAlignment="Top" Width="124"/>
+        <Label x:Name="lblNbItemsInGrid" Content="0" HorizontalAlignment="Left" VerticalAlignment="Top" Margin="506,400,0,0" Width="66"/>
+        <Label Content="Number of Items in Grid:" HorizontalAlignment="Left" Margin="353,400,0,0" VerticalAlignment="Top" Width="148"/>
+        <Label Content="Selected:" HorizontalAlignment="Left" Margin="591,400,0,0" VerticalAlignment="Top"/>
+        <Label x:Name="lblNumberItemsSelected" Content="0" HorizontalAlignment="Left" Margin="650,400,0,0" VerticalAlignment="Top" Width="67"/>
+        <TextBox x:Name="txtResultSize" HorizontalAlignment="Left" Height="23" Margin="224,104,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="124" Text="100"/>
+        <TextBlock HorizontalAlignment="Left" Margin="95,95,0,0" TextWrapping="Wrap" Text="ResultSize (aka Nb of mailboxes to display):" VerticalAlignment="Top" Width="124"/>
+        <Label Content="Status:" HorizontalAlignment="Left" Margin="0,447,0,0" VerticalAlignment="Top"/>
+        <TextBox x:Name="txtMainCommand" HorizontalAlignment="Left" Height="132" Margin="10,169,0,0" TextWrapping="Wrap" Text="Get-Mailbox command to be run..." VerticalAlignment="Top" Width="338" IsReadOnly="True"/>
+        <Rectangle HorizontalAlignment="Left" Height="26" Margin="353,400,0,0" VerticalAlignment="Top" Width="232">
+            <Rectangle.Stroke>
+                <SolidColorBrush Color="{DynamicResource {x:Static SystemColors.ActiveBorderColorKey}}"/>
+            </Rectangle.Stroke>
+        </Rectangle>
+        <Rectangle HorizontalAlignment="Left" Height="26" Margin="590,400,0,0" VerticalAlignment="Top" Width="173">
+            <Rectangle.Stroke>
+                <SolidColorBrush Color="{DynamicResource {x:Static SystemColors.ActiveBorderColorKey}}"/>
+            </Rectangle.Stroke>
+        </Rectangle>
+        <Label Content="The command run when clicking on the Search button is:" HorizontalAlignment="Left" Margin="10,143,0,0" VerticalAlignment="Top" Width="338" FontStyle="Italic"/>
 
     </Grid>
 </Window>
-
 "@
+
 $inputXMLClean = $inputXML -replace 'mc:Ignorable="d"','' -replace "x:N",'N' -replace 'x:Class=".*?"','' -replace 'd:DesignHeight="\d*?"','' -replace 'd:DesignWidth="\d*?"',''
 [xml]$xaml = $inputXMLClean
 $reader = New-Object System.Xml.XmlNodeReader $xaml
@@ -450,6 +482,7 @@ $wpf.btnAction.add_Click({
 #Things to load when the WPF form is loaded aka in memory
 $Wpf.$FormName.Add_Loaded({
     Ready-Label
+    Update-MainCommandLine
 })
 #Things to load when the WPF form is rendered aka drawn on screen
 $Wpf.$FormName.Add_ContentRendered({
@@ -474,6 +507,12 @@ $wpf.GridView.add_SelectionChanged({
         $wpf.comboSelectAction.IsEnabled = $true
     }
     $wpf.lblNumberItemsSelected.Content = $Selected
+})
+$wpf.txtMailboxString.add_TextChanged({
+    Update-MainCommandLine
+})
+$wpf.txtResultSize.add_TextChanged({
+    Update-MainCommandLine
 })
 #End of Text Changed events
 #endregion
